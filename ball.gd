bfbcdcbead123771d15af81f282acb09b065e569
jluini <jluini@gmail.com>
2025-11-16 20:01:37 -0300

    Detectar quietud

--- ball.gd
+++ ball.gd
@@ -1,13 +1,1 @@
 extends RigidBody2D
-
-func _ready():
-	print("estoy ready")
-
-func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	pass
-	# state.set_linear_velocity(Vector2.ZERO)
-	
-	#if Input.is_action_pressed("ui_left"):
-		#state.apply_torque(-10000)
-	#elif Input.is_action_pressed("ui_right"):
-		#state.apply_torque(10000)
--- game.gd
+++ game.gd
@@ -2,7 +2,7 @@ extends Control
 
 enum State {
 	NO_GAME,
-	
+
 	STARTING,
 	PROCESSING,
 	PLAYING,
@@ -13,44 +13,75 @@ enum State {
 @export var map : PackedScene
 @export var player : PackedScene
 
-### 
+###
 
 var state : State
 
-### 
+###
 
 func _ready() -> void:
-	state = State.NO_GAME
+	set_state(State.NO_GAME)
+
+func set_state(new_state):
+	print(State.find_key(state), " -> ", State.find_key(new_state))
 	
-	# print(State)
-	# print(State.keys())
+	self.state = new_state
 	
-
-func _input(event):
-	if event.is_action_pressed("ui_accept"):
-		print("Accept")
-		if state == State.NO_GAME:
-			state = State.STARTING
-			_start_simulation(%simulation)
-				
-	elif event.is_action_pressed("ui_select"):
-		print("Select")
-
+func _input(event: InputEvent):
+	match state:
+		State.NO_GAME:
+			if event.is_action_pressed("ui_accept"):
+				set_state(State.STARTING)
+				_start_simulation()
+		State.PLAYING:
+			pass
+			
 
 
-func _start_simulation(container: Node):
-	for child in container.get_children():
+
+
+
+func _check_sleeping(source):
+	var is_moving = false
+	for do in %simulation.get_node("map/map/dynamic_objects").get_children():
+		if !do.sleeping:
+			is_moving = true
+			break
+	
+	for do in %simulation.get_node("units").get_children():
+		if !do.sleeping:
+			is_moving = true
+			break
+			
+	if not is_moving:
+		match state:
+			State.STARTING:
+				set_state(State.PROCESSING)
+
+func _start_simulation():
+	for child in %simulation.get_node("map").get_children():
+		child.free()
+		
+	for child in %simulation.get_node("units").get_children():
 		child.free()
 	
 	var scenario = map.instantiate()
-	container.add_child(scenario)
+		
+	for do in scenario.get_node("dynamic_objects").get_children():
+		do.connect("sleeping_state_changed", self._check_sleeping.bind(do))
 	
-	var turtle1 = player.instantiate()
-	var turtle2 = player.instantiate()
+	%simulation.get_node("map").add_child(scenario)
+
+	add_a_turtle(scenario.get_node('spawn0').position, "turtle1")
+	add_a_turtle(scenario.get_node('spawn1').position, "turtle2")
 	
-	container.add_child(turtle1)
-	container.add_child(turtle2)
+
+func add_a_turtle(position, name):
+	var new_turtle = player.instantiate()
+	new_turtle.connect("sleeping_state_changed", self._check_sleeping.bind(new_turtle))
+	%simulation.get_node("units").add_child(new_turtle)
+
+	new_turtle.position = position
+	new_turtle.name = name
 	
-	turtle1.position = scenario.get_node('spawn0').position
-	turtle2.position = scenario.get_node('spawn1').position
-	
+	return new_turtle
