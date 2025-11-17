1c73af9af9412242fa5f24553f3f875460e6e148
jluini <jluini@gmail.com>
2025-11-16 23:19:28 -0300

    Alternar turnos

--- game.gd
+++ game.gd
@@ -15,11 +15,19 @@ enum State {
 
 ###
 
+var local_player = preload("res://local_player.gd")
+
+###
+
 var state : State
+var turn = 0
+var players = []
+var current_player = null
 
 ###
 
 func _ready() -> void:
+	%timer.connect("timeout", _on_timer_timeout)
 	set_state(State.NO_GAME)
 
 func set_state(new_state):
@@ -34,12 +42,31 @@ func _input(event: InputEvent):
 				set_state(State.STARTING)
 				_start_simulation()
 		State.PLAYING:
-			pass
+			if event.is_action_pressed("ui_left"):
+				current_player.going_left()
+			elif event.is_action_pressed("ui_right"):
+				current_player.going_right()
+			elif event.is_action_released("ui_left"):
+				current_player.standing_still()
+			elif event.is_action_released("ui_right"):
+				current_player.standing_still()
 			
 
 
+# Signal handlers
 
-
+func _on_timer_timeout():
+	match state:
+		State.PLAYING:
+			current_player.standing_still()
+			set_state(State.WAITING)
+	
+			%timer.start()
+		State.WAITING:
+			turn = (turn % 2) + 1
+			current_player = players[turn - 1]
+			_continue_game()
+		
 
 func _check_sleeping(source):
 	var is_moving = false
@@ -52,11 +79,30 @@ func _check_sleeping(source):
 		if !do.sleeping:
 			is_moving = true
 			break
-			
+
 	if not is_moving:
+		print("DONE ", source.name)
 		match state:
 			State.STARTING:
-				set_state(State.PROCESSING)
+				_start_game()
+	else:
+		print("---- ", source.name)
+
+func _start_game():
+	turn = 1
+	current_player = players[turn - 1]
+	_continue_game()
+
+func _continue_game():
+	if _game_goes_on():
+		set_state(State.PLAYING)
+		%timer.start()
+	else:
+		pass # TODO
+
+func _game_goes_on():
+	return true # TODO
+	
 
 func _start_simulation():
 	for child in %simulation.get_node("map").get_children():
@@ -72,9 +118,16 @@ func _start_simulation():
 	
 	%simulation.get_node("map").add_child(scenario)
 
-	add_a_turtle(scenario.get_node('spawn0').position, "turtle1")
-	add_a_turtle(scenario.get_node('spawn1').position, "turtle2")
+	# TODO: delete old players
 	
+	var player1 = local_player.new()
+	player1.units.append(add_a_turtle(scenario.get_node('spawn0').position, "turtle1"))
+	
+	var player2 = local_player.new()
+	player2.units.append(add_a_turtle(scenario.get_node('spawn1').position, "turtle2"))
+	
+	players.append(player1)
+	players.append(player2)
 
 func add_a_turtle(position, name):
 	var new_turtle = player.instantiate()
